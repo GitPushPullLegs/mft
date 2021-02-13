744f4655b2a22dc3f5b840dbcd1b1d3915bb3e00
Jose Aguilar <jose.aguilar.6694@gmail.com>
2021-02-12 23:03:13 -0800

    Eliminated unnecessary context manager.

--- mft/client.py
+++ mft/client.py
@@ -58,28 +58,27 @@ class Client:
 
     def _create_file_share(self, expiry: int, password: str = None):
         """Creates a file share in MFT and returns the url and token. Expiration defaults to a month from run."""
-        with self.session as session:
-            session.headers.update({'X-CSRF-Token': self.csrf_token})
+        self.session.headers.update({'X-CSRF-Token': self.csrf_token})
 
-            payload = {
-                "ShareType": 1,
-                "RecipientEmailAddress": "",
-                "SenderName": self.credentials['user'].split("@")[0],
-                "SenderEmail": self.credentials['user'],
-                "NotifyUserOnGuestTransfer": 1,
-                "SenderCarbonCopy": 0,
-                "EmailSubject": "MCS MFT",
-                "EmailBody": "",
-                "ExpirationTimestamp": expiry,
-                "PasswordIsSet": 0 if not password else 1,
-                "Password": '' if not password else password,
-                "IncludePasswordInEmail": 0,
-                "MaxFileSize": 0
-            }
+        payload = {
+            "ShareType": 1,
+            "RecipientEmailAddress": "",
+            "SenderName": self.credentials['user'].split("@")[0],
+            "SenderEmail": self.credentials['user'],
+            "NotifyUserOnGuestTransfer": 1,
+            "SenderCarbonCopy": 0,
+            "EmailSubject": "MCS MFT",
+            "EmailBody": "",
+            "ExpirationTimestamp": expiry,
+            "PasswordIsSet": 0 if not password else 1,
+            "Password": '' if not password else password,
+            "IncludePasswordInEmail": 0,
+            "MaxFileSize": 0
+        }
 
-            response = session.post(urljoin(self.host, r"Web%20Client/Share/CreateFileShare.xml?Command=CreateFileShare"), data=payload)
-            return {"url": re.findall(r"(?<=<ShareURL>)[\W\w]+(?=<\/ShareURL>)", response.text)[0],  # ShareURL Encoded
-                    "token": re.findall(r"(?<=<ShareToken>)[\W\w]+(?=<\/ShareToken>)", response.text)[0]}
+        response = self.session.post(urljoin(self.host, r"Web%20Client/Share/CreateFileShare.xml?Command=CreateFileShare"), data=payload)
+        return {"url": re.findall(r"(?<=<ShareURL>)[\W\w]+(?=<\/ShareURL>)", response.text)[0],  # ShareURL Encoded
+                "token": re.findall(r"(?<=<ShareToken>)[\W\w]+(?=<\/ShareToken>)", response.text)[0]}
 
     def _upload_files(self, files: [str], token: str):
         """Uploads the files to the previously created file share."""
