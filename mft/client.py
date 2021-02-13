cbaa4ec1c1db17a34f12c67705d926ec56a27f10
Jose Aguilar <jose.aguilar.6694@gmail.com>
2021-02-13 15:04:53 -0800

    Basic reformat of Client.

--- mft/client.py
+++ mft/client.py
@@ -1,14 +1,15 @@
 import re
 import time
 from collections import deque
-from urllib.parse import urlsplit, unquote, urljoin
 from datetime import datetime, timedelta
+from urllib.parse import urlsplit, unquote, urljoin
 
 import requests
 
 
 class Client:
-    _HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+    _HEADERS = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
 
     def __init__(self, host: str):
         """
@@ -42,13 +43,13 @@ class Client:
         self.session.get(self.host)
         return self.visit_history[-1].status_code == 200 and self.visit_history[-1].url == self.host
 
-
-    def _event_hooks(self, r ,*args, **kwargs):
+    def _event_hooks(self, r, *args, **kwargs):
         scheme, netloc, path, query, frag = urlsplit(r.url)
         print(r.url, r.status_code)
         if path == '/' and r.status_code == 200:
             self.session.cookies.update(r.cookies.get_dict())
-            self.session.post(urljoin(self.host, fr"Web%20Client/Login.xml?Command=Login&Sync={int(time.time())}"), data=self.credentials)
+            self.session.post(urljoin(self.host, fr"Web%20Client/Login.xml?Command=Login&Sync={int(time.time())}"),
+                              data=self.credentials)
         elif path == '/Web%20Client/Login.xml' and r.status_code == 200:
             self.session.get(urljoin(self.host, r"Web%20Client/Share/Console.htm"))
             self.csrf_token = re.findall(r"(?<=<CsrfToken>)[\W\w]+(?=<\/CsrfToken>)", r.text)[0]
@@ -57,7 +58,8 @@ class Client:
             self.visit_history.append(r)
             return r
 
-    def upload_files(self, files: [str], expiry: int = int((datetime.now() + timedelta(days=30)).timestamp()), password: str = None) -> str:
+    def upload_files(self, files: [str], expiry: int = int((datetime.now() + timedelta(days=30)).timestamp()),
+                     password: str = None) -> str:
         """
         Uploads the files to the MFT server and returns the URL to be shared with the recipient.
         :param files: A list of the files to be shared with this link.
@@ -91,13 +93,14 @@ class Client:
             "MaxFileSize": 0
         }
 
-        response = self.session.post(urljoin(self.host, r"Web%20Client/Share/CreateFileShare.xml?Command=CreateFileShare"), data=payload)
+        response = self.session.post(
+            urljoin(self.host, r"Web%20Client/Share/CreateFileShare.xml?Command=CreateFileShare"), data=payload)
         return {"url": re.findall(r"(?<=<ShareURL>)[\W\w]+(?=<\/ShareURL>)", response.text)[0],  # ShareURL Encoded
                 "token": re.findall(r"(?<=<ShareToken>)[\W\w]+(?=<\/ShareToken>)", response.text)[0]}
 
     def _upload_files(self, files: [str], token: str):
         """Uploads the files to the previously created file share."""
         for index, file in enumerate(files):
-            self.session.post(urljoin(self.host, fr"Web%20Client/Share/MultipleFileUploadResult.htm?Command=UploadFileShare&TransferID={index + 1}&File={file}&ShareToken={token}&IsVirtual=0&CsrfToken={self.csrf_token}"),
+            self.session.post(urljoin(self.host,
+                                      fr"Web%20Client/Share/MultipleFileUploadResult.htm?Command=UploadFileShare&TransferID={index + 1}&File={file}&ShareToken={token}&IsVirtual=0&CsrfToken={self.csrf_token}"),
                               files={"file": open(file, 'rb')})
-
