aa4c2fcea5f812d88ffc7d74d371fa48d6627a62
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-16 10:38:44 -0800

    Updated distribution.

--- setup.py
+++ setup.py
@@ -9,13 +9,17 @@ setup(
     author='Joe Aguilar',
     author_email='Jose.Aguilar.6694@gmail.com',
     license='GNU General Public License',
-    packages=find_packages(),
+    packages=['mft'],
     install_requires=['requests', 'urllib3'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
