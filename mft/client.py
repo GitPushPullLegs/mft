ab0b81f571d7b5aad0e059074dbe6d9e4a1d7773
Jose Aguilar <jose.aguilar.6694@gmail.com>
2021-02-18 23:21:53 -0800

    Added functionality to request files.

--- mft/client.py
+++ mft/client.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 from urllib.parse import urlsplit, unquote, urljoin, quote
 import urllib3
 import os
+import enum
+import warnings
 
 import requests
 
@@ -69,10 +71,16 @@ class Client:
             self.visit_history.append(r)
             return r
 
-    def create_file_share(self, files: [str], expiry: int = int((datetime.now() + timedelta(days=30)).timestamp()),
+    class ShareType(enum.Enum):
+        request = 0
+        send = 1
+
+    def create_file_share(self, share_type: ShareType, files: [str] = None,
+                          expiry: int = int((datetime.now() + timedelta(days=30)).timestamp()),
                           password: str = None, subject: str = "File Share", comments: str = None) -> str:
         """
         Uploads the files to the MFT server and returns the URL to be shared with the recipient.
+        :param share_type: Whether you are requesting files or sending them.
         :param comments: A comment to attach to the file share.
         :param subject: The subject of the file share.
         :param files: A list of the files to be shared with this link.
@@ -80,14 +88,21 @@ class Client:
         :param password: An optional password to protect the files.
         :return: The link to the files.
         """
-        data = self._create_file_share(subject=subject, comments=comments, expiry=expiry, password=password)
-        self._upload_files(files=files, token=data['token'])
+        if share_type is Client.ShareType.send and (not files or not isinstance(files, list)):
+            raise AttributeError("If sending files, the files parameter must contain a list of files.")
+        elif share_type is Client.ShareType.request and files:
+            warnings.warn("You are requesting files but submitted files. They will be ignored.")
+
+        data = self._create_file_share(share_type=share_type.value, subject=subject, comments=comments, expiry=expiry,
+                                       password=password)
+        if share_type is Client.ShareType.send:
+            self._upload_files(files=files, token=data['token'])
         return data['url']
 
-    def _create_file_share(self, subject: str, comments: str, expiry: int, password: str = None):
+    def _create_file_share(self, share_type: int, subject: str, comments: str, expiry: int, password: str = None):
         """Creates a file share in MFT and returns the url and token. Expiration defaults to a month from run."""
         payload = {
-            "ShareType": 1,
+            "ShareType": share_type,
             "RecipientEmailAddress": "",
             "SenderName": self.credentials['user'].split("@")[0],
             "SenderEmail": self.credentials['user'],
